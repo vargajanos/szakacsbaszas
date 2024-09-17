6fb785a074e684bf06967a5091ed90af027eaec1
Csoki <csokaandras@turr.hu>
2024-09-17 11:23:00 +0200

    Alapozás

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+var mysql = require('mysql');
+const uuid = require('uuid');
+var cors = require('cors');
+var CryptoJS = require("crypto-js");
+
+const app = express();
+const port = process.env.PORT;
+//const passwdRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+var pool  = mysql.createPool({
+  connectionLimit : process.env.CONNECTIONLIMIT,
+  host            : process.env.DBHOST,
+  user            : process.env.DBUSER,
+  password        : process.env.DBPASS,
+  database        : process.env.DBNAME
+});
+
+app.listen(port, () => {
+    //console.log(process.env) ;
+    console.log(`A masinéria megfigyel itten e: ${port}...`);
+  });
