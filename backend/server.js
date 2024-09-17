0d7a8ea0c3490e14341b05cd6f8d1ff31ad4314f
Csoki <csokaandras@turr.hu>
2024-09-17 12:28:37 +0200

    regisztráció

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,50 @@ var pool  = mysql.createPool({
   database        : process.env.DBNAME
 });
 
+app.post("/reg", (req, res) => {
+
+  // Minden szükséges adat megvan-e?
+  if (!req.body.name || !req.body.email || !req.body.passwd || !req.body.confirm ){
+    res.status(203).send('Nem adtál meg minden kötelező adatot!');
+    return;
+  }
+
+  // jelszavak ellenőrzése
+  if (req.body.passwd != req.body.confirm){
+    res.status(203).send('A megadott jelszavak nem egyeznek!');
+    return;
+  }
+
+  pool.query(`SELECT * FROM users WHERE email='${req.body.email}'`, (err, results) => {
+     
+    if (err){
+      res.status(500).send('Hiba történt az adatbázis elérése közben!');
+      return;
+     }
+    
+    // ha van már ilyen email cím
+    if (results.length != 0){
+      res.status(203).send('Ez az e-mail cím már regisztrálva van!');
+      return;
+     }
+    
+    // új felhasználó felvétele
+    pool.query(`INSERT INTO users VALUES('${uuid.v4()}', '${req.body.name}', '${req.body.email}', '${req.body.passwd}', 'user', '0', '')`, (err, results)=>{
+      if (err){
+        res.status(500).send('Hiba történt az adatbázis művelet közben!');
+        return;
+       }
+       res.status(202).send('Sikeres regisztráció!');
+       return;
+    });
+    return;
+  });
+
+})
+
+
 app.listen(port, () => {
     //console.log(process.env) ;
     console.log(`A masinéria megfigyel itten e: ${port}...`);
   });
+
